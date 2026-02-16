bd95be392a29e631d37f55669fbdec01cfba0b2d
HarshithaSabbinei <harshithasabbineni@gmail.com>
2026-02-16 09:47:37 +0530

    Initialize Angular application with bootstrap

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideZoneChangeDetection } from '@angular/core';
+import { AppComponent } from './app/app';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true })
+  ]
+}).catch(err => console.error(err));
